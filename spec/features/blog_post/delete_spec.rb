d682442bf19dbbe7df08b73972fe12f28d68ac4e
Karan Mehta <79548116+karanm645@users.noreply.github.com>
2023-06-22 17:04:32 -0500

    "delete button created, refactor controller"

---
+++ spec/features/blog_post/delete_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe "Delete Blog Post" do 
+  describe 'when the user goes to edit page' do
+    it 'has a delete button for article' do       
+      my_blog_post = BlogPost.create!(title: "3rd blog post", body: "this is an old body")
+
+      visit "/blog_posts/#{my_blog_post.id}/edit"
+
+      expect(page).to have_content("Delete")
+      
+      click_button "Delete"
+      
+      visit root_path
+
+      expect(page).to_not have_content("3rd blog post")
+    end 
+  end 
+end 
