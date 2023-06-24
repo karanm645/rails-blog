851cf762561d0970d0b5f1ed11af700545fb626b
Karan Mehta <79548116+karanm645@users.noreply.github.com>
2023-06-24 10:09:27 -0500

    "create model tests for scheduled, published, and draft blog posts"

--- spec/features/blog_post/delete_spec.rb
+++ spec/features/blog_post/delete_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 RSpec.describe "Delete Blog Post" do 
   describe 'when the user goes to edit page' do
+    before do
+      @user = User.create!(email: 'test@example.com', password: 'password')
+    end
     it 'has a delete button for article' do       
       my_blog_post = BlogPost.create!(title: "3rd blog post", body: "this is an old body")
-
       visit "/blog_posts/#{my_blog_post.id}/edit"
-
       expect(page).to have_content("Delete")
-      
       click_button "Delete"
       
       visit root_path
--- spec/models/blog_post.rb
+++ spec/models/blog_post.rb
@@ -5,4 +5,60 @@ RSpec.describe BlogPost, type: :model do
     it { should validate_presence_of(:title)}
     it { should validate_presence_of(:body)}
   end 
+
+  describe "blog post mode" do 
+    it "draft? returns true for draft blog post" do 
+      post = BlogPost.new(published_at: nil).draft?
+
+      expect(post).to be true
+    end 
+
+    it "draft? returns false for published post" do 
+      post = BlogPost.new(published_at: 1.year.ago).draft?
+
+      expect(post).to_not be true
+    end 
+
+    it "draft? returns false for scheduled post" do 
+      post = BlogPost.new(published_at: 1.year.from_now).draft?
+
+      expect(post).to_not be true
+    end 
+
+    it "published? returns false for draft post" do 
+      post = BlogPost.new(published_at: nil).published?
+
+      expect(post).to_not be true
+    end 
+
+    it "published? returns true for published post" do 
+      post = BlogPost.new(published_at: 1.year.ago).published?
+
+      expect(post).to be true
+    end 
+
+    it "published? returns false for scheduled post" do 
+      post = BlogPost.new(published_at: 1.year.from_now).published?
+
+      expect(post).to_not be true
+    end 
+
+    it "scheduled? returns false for draft post" do 
+      post = BlogPost.new(published_at: nil).scheduled?
+
+      expect(post).to_not be true
+    end 
+
+    it "scheduled? returns false for published post" do 
+      post = BlogPost.new(published_at: 1.year.ago).scheduled?
+
+      expect(post).to_not be true
+    end 
+
+    it "scheduled? returns true for scheduled post" do 
+      post = BlogPost.new(published_at: 1.year.from_now).scheduled?
+      
+      expect(post).to be true
+    end 
+  end 
 end 
